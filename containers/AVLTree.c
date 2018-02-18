f21c9b2f6e913376b6a13cf33cb9f097d98bb413
klx3300 <inc19991@gmail.com>
2018-02-18 18:33:40 +0800

    completed avl insert

--- containers/AVLTree.c
+++ containers/AVLTree.c
@@ -128,24 +128,24 @@ void qAVLTree__list_insert_right(qAVLTreeNode* parent,qAVLTreeNode* child){
 }
 
 // parent: where do i come from?
-int qAVLTree__recursive_insert(qAVLTreeDescriptor desc,qAVLTreeNode* root,void* elem,ui size){
+qAVLTreeNode* qAVLTree__recursive_insert(qAVLTreeDescriptor desc,qAVLTreeNode* root,void* elem,ui size){
     int cmpresult = (desc.comp(elem,size,root->data,root->size));
     if(cmpresult == 0){
         // unfortunately, already exists.
-        return -1;
+        return NULL;
     }else if(cmpresult < 0){
         if(root->lchild == NULL){
             // well, you there then!
             qAVLTreeNode* tmpnd = malloc(sizeof(qAVLTreeNode));
             if(tmpnd == NULL){
                 SETERR(ZHWK_ERR_MM_ALLOC_FAIL);
-                return -1;
+                return NULL;
             }
             void* cpdata = malloc(size);
             if(cpdata == NULL){
                 SETERR(ZHWK_ERR_MM_ALLOC_FAIL);
                 free(tmpnd);
-                return -1;
+                return NULL;
             }
             // allocation success!
             ZEROINIT(*tmpnd);
@@ -153,28 +153,96 @@ int qAVLTree__recursive_insert(qAVLTreeDescriptor desc,qAVLTreeNode* root,void* 
             tmpnd->size = size;
             tmpnd->data = cpdata;
             // complete insertion
-            qAVLTree__list_insert_left(root,cpdata);
-            root->lchild = cpdata;
+            qAVLTree__list_insert_left(root,tmpnd);
+            root->lchild = tmpnd;
             // update balance factor
             root->blfactor=root->blfactor-1;
-            return 0;
+            return root;
         }
-        int insresult = qAVLTree__recursive_insert(desc,root->lchild,elem,size);
-        if(insresult == -1){
-            return -1;
+        qAVLTreeNode* insresult = qAVLTree__recursive_insert(desc,root->lchild,elem,size);
+        if(insresult == NULL){
+            return NULL;
         }
         // then the insertion is succeeded.
+        // update root
+        root->lchild = insresult;
         // update blfactor
         root->blfactor=root->blfactor-1;
-        if(root->blfactor <= -1){
+        if(root->blfactor <= -2){
             // trigger rotation
+            if(root->lchild->rchild == NULL || root->lchild->rchild->blfactor<=0){
+                // in this case ,simple right rotate is good enough.
+                return qAVLTree__SimpleRightRotate(root);
+            }else{
+                return qAVLTree__LeftRightRotate(root);
+            }
         }
-        return 1;
+        return root;
     }else{
-
+        if(root->rchild == NULL){
+            qAVLTreeNode* tmpnd = malloc(sizeof(qAVLTreeNode));
+            if(tmpnd == NULL){
+                SETERR(ZHWK_ERR_MM_ALLOC_FAIL);
+                return NULL;
+            }
+            void* cpdata = malloc(size);
+            if(cpdata == NULL){
+                SETERR(ZHWK_ERR_MM_ALLOC_FAIL);
+                free(tmpnd);
+                return NULL;
+            }
+            ZEROINIT(*tmpnd);
+            memcpy(cpdata,elem,size);
+            tmpnd->size = size;
+            tmpnd->data = cpdata;
+            qAVLTree__list_insert_right(root,tmpnd);
+            root->rchild = tmpnd;
+            root->blfactor = root->blfactor+1;
+            return root;
+        }
+        qAVLTreeNode* insresult = qAVLTree__recursive_insert(desc,root->rchild,elem,size);
+        if(insresult == NULL){
+            return NULL;
+        }
+        root->rchild = insresult;
+        root->blfactor = root->blfactor+1;
+        if(root->blfactor >= 2){
+            if(root->rchild->lchild != NULL || root->rchild->lchild->blfactor >= 0){
+                return qAVLTree__SimpleLeftRotate(root);
+            }else{
+                return qAVLTree__RightLeftRotate(root);
+            }
+        }
+        return root;
     }
 }
 
 int qAVLTree__insert(qAVLTreeDescriptor *desc,void* elem,ui size){
-
+    if(desc->root == NULL){
+        // the very first elem
+        desc->root = malloc(sizeof(qAVLTreeNode));
+        if(desc->root == NULL){
+            SETERR(ZHWK_ERR_MM_ALLOC_FAIL);
+            return -1;
+        }
+        void* cpdata = malloc(size);
+        if(cpdata == NULL){
+            SETERR(ZHWK_ERR_MM_ALLOC_FAIL);
+            free(desc->root);
+            desc->root = NULL;
+            return -1;
+        }
+        ZEROINIT(*(desc->root));
+        memcpy(cpdata,elem,size);
+        desc->root->data = cpdata;
+        desc->root->size = size;
+        desc->size = 1;
+        return 0;
+    }
+    // recursive version kick in
+    qAVLTreeNode* retv = qAVLTree__recursive_insert(*desc,desc->root,elem,size);
+    if(retv == NULL) return -1;
+    desc->size=desc->size+1;
+    desc->root = retv;
+    return 0;
 }
